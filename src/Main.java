b50e254b4bbfe5d29b447c846f2ce1d8bfb57d31
baranbicher <baranbicer0103@outlook.com>
2023-05-25 15:36:37 +0300

    Kdv programı güncel ve taslak oranlara göre hazırlandı.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner Fiyat = new Scanner(System.in);
-        double urunfiyat, kdvOranı = 0.18, kdvOranı1 = 0.08;
+        double urunfiyat, kdvOrani = 0.18, kdvOrani1 = 0.08;
         System.out.print("Ürünün Fiyatını Girin:");
         urunfiyat = Fiyat.nextDouble();
         double kdvSonuc = urunfiyat <=1000 ? urunfiyat*0.18 : urunfiyat*0.08;
         double kdvliTutar = urunfiyat + kdvSonuc;
 
         System.out.println("KDV'siz Tutar: " + urunfiyat);
-        System.out.println("KDV Oranları. " + kdvOranı + " " + kdvOranı1);
+        System.out.println("KDV Oranları. " + kdvOrani + " " + kdvOrani1);
         System.out.println("Ürünün KDV'si: " + kdvSonuc);
         System.out.println("Ürünün KDV'li Tutarı: " + kdvliTutar);
     }
